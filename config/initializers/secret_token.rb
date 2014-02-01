fcd98fcdf5a5d4789758d9fb152efbfead701991
Jeremy Lubin <lubin.jeremy@gmail.com>
2014-02-01 11:41:06 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flowers::Application.config.secret_key_base = '8597347e55145fbf2bad6ebd9844183be4a50240405e739fb7890aec6252a407bd2cf7d115a78fe1787634766b25265e1f9c9fe77f36f32f5522ab0b358937f8'
